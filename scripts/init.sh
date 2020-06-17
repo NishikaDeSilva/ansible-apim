b38bbc98e55ecdb461746bbde015009d2c6dd00f
NishikaDeSilva <desilvanishika73@gmail.com>
2020-06-17 19:48:07 +0530

    Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -24,6 +24,8 @@ aws --version
 aws configure set aws_access_key_id $AWS_ACCESS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET_KEY
 
+aws configure list
+
 #install wum 
 wget -q http://product-dist.wso2.com/downloads/wum/3.0.6/wum-3.0.6-linux-x64.tar.gz
 tar -C /usr/local -xzf wum-3.0.6-linux-x64.tar.gz
@@ -39,12 +41,10 @@ wum add $PRODUCT-$VERSION -y
 cp $HOME/.wum3/products/$PRODUCT/$VERSION/$PRODUCT-$VERSION.zip $WORKSPACE/ansible-apim/files/packs/$PRODUCT-$VERSION.zip
 
 # install ansible
-if !which ansible; then 
-    apt update
-    apt install software-properties-common -y
-    apt-add-repository --yes --update ppa:ansible/ansible
-    apt install ansible -y
-fi
+apt update
+apt install software-properties-common -y
+apt-add-repository --yes --update ppa:ansible/ansible
+apt install ansible -y
 
 wget -q https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz -P $WORKSPACE/ansible-apim/files/lib
 wget -q https://download.jar-download.com/cache_jars/mysql/mysql-connector-java/5.1.49/jar_files.zip
