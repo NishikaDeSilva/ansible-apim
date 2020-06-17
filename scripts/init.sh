8bcc171589b914665d53497530b33e60f6be80fd
NishikaDeSilva <desilvanishika73@gmail.com>
2020-06-17 18:37:59 +0530

    Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,11 +2,25 @@
 
 WUM_USER=$1
 WUM_PASS=$2
+
+AWS_ACCESS_KEY=$3
+AWS_SECRET_KEY=$4
+
 PRODUCT="wso2am"
 VERSION="2.1.0"
 
 WORKSPACE=$(pwd)
 echo $WORKSPACE
+
+# install aws
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+unzip awscliv2.zip
+sudo ./aws/install
+
+aws --version
+
+aws configure set aws_access_key_id $AWS_ACCESS_KEY
+aws configure set aws_secret_access_key $AWS_SECRET_KEY
 
 #install wum 
 wget http://product-dist.wso2.com/downloads/wum/3.0.6/wum-3.0.6-linux-x64.tar.gz
@@ -20,13 +34,15 @@ wum init -u $WUM_USER -p $WUM_PASS
 # add product
 wum add $PRODUCT-$VERSION
 
-cp $HOME/.wum3/products/$PRODUCT/$VERSION/$PRODUCT-$VERSION.zip $WORKSPACE/ansible-apim/files/lib/packs/$PRODUCT-$VERSION.zip
+cp $HOME/.wum3/products/$PRODUCT/$VERSION/$PRODUCT-$VERSION.zip $WORKSPACE/ansible-apim/files/packs/$PRODUCT-$VERSION.zip
 
 # install ansible
-apt update
-apt install software-properties-common
-apt-add-repository --yes --update ppa:ansible/ansible
-apt install ansible
+if !which ansible; then 
+    apt update
+    apt install software-properties-common
+    apt-add-repository --yes --update ppa:ansible/ansible
+    apt install ansible
+fi
 
 wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz -P $WORKSPACE/ansible-apim/files/lib
 wget https://download.jar-download.com/cache_jars/mysql/mysql-connector-java/5.1.49/jar_files.zip
