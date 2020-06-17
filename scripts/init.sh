3a851ba2c88e63747ff3f4f604dafb0da4220f8c
NishikaDeSilva <desilvanishika73@gmail.com>
2020-06-17 19:27:36 +0530

    Update

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -o xtrace; set -e
+
 WUM_USER=$1
 WUM_PASS=$2
 
@@ -23,7 +25,7 @@ aws configure set aws_access_key_id $AWS_ACCESS_KEY
 aws configure set aws_secret_access_key $AWS_SECRET_KEY
 
 #install wum 
-wget http://product-dist.wso2.com/downloads/wum/3.0.6/wum-3.0.6-linux-x64.tar.gz
+wget -q http://product-dist.wso2.com/downloads/wum/3.0.6/wum-3.0.6-linux-x64.tar.gz
 tar -C /usr/local -xzf wum-3.0.6-linux-x64.tar.gz
 export PATH=$PATH:/usr/local/wum/bin
 
@@ -32,18 +34,18 @@ which wum
 wum init -u $WUM_USER -p $WUM_PASS
 
 # add product
-wum add $PRODUCT-$VERSION
+wum add $PRODUCT-$VERSION -y
 
 cp $HOME/.wum3/products/$PRODUCT/$VERSION/$PRODUCT-$VERSION.zip $WORKSPACE/ansible-apim/files/packs/$PRODUCT-$VERSION.zip
 
 # install ansible
 if !which ansible; then 
     apt update
-    apt install software-properties-common
+    apt install software-properties-common -y
     apt-add-repository --yes --update ppa:ansible/ansible
-    apt install ansible
+    apt install ansible -y
 fi
 
-wget https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz -P $WORKSPACE/ansible-apim/files/lib
-wget https://download.jar-download.com/cache_jars/mysql/mysql-connector-java/5.1.49/jar_files.zip
-unzip jar_files.zip -d $WORKSPACE/ansible-apim/files/lib
+wget -q https://github.com/AdoptOpenJDK/openjdk8-binaries/releases/download/jdk8u252-b09/OpenJDK8U-jdk_x64_linux_hotspot_8u252b09.tar.gz -P $WORKSPACE/ansible-apim/files/lib
+wget -q https://download.jar-download.com/cache_jars/mysql/mysql-connector-java/5.1.49/jar_files.zip
+unzip -q jar_files.zip -d $WORKSPACE/ansible-apim/files/lib
